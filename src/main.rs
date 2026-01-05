8076a20753ed8f2d87c92c98e9e510ff40a79852
simon0302010 <simon0302010@gmail.com>
2026-01-05 21:28:03 +0100

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::process;
+use std::{
+    io::{BufRead, BufReader},
+    process::{self, Stdio},
+    thread,
+};
 
 fn main() {
     let mut args = std::env::args().collect::<Vec<String>>();
@@ -9,23 +13,63 @@ fn main() {
         process::exit(1);
     }
 
-    let output = process::Command::new(args.remove(0))
+    let mut child = process::Command::new(args.remove(0))
         .args(args)
-        .output()
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
         .unwrap_or_else(|e| {
             eprintln!("Failed to start child process: {}", e);
             process::exit(1);
         });
 
-    let utf8out = String::from_utf8(output.stdout)
-        .expect("Failed to decode stdout")
+    let stdout = child.stdout.take().expect("Failed to take stdout");
+    let stderr = child.stderr.take().expect("Failed to take stderr");
+
+    let out_handle = thread::spawn(move || {
+        let mut buf = String::new();
+        for line in BufReader::new(stdout).lines().flatten() {
+            println!("{}", line);
+            buf.push_str(&line);
+            buf.push('\n');
+        }
+        buf
+    });
+
+    let err_handle = thread::spawn(move || {
+        let mut buf = String::new();
+        for line in BufReader::new(stderr).lines().flatten() {
+            println!("{}", line);
+            buf.push_str(&line);
+            buf.push('\n');
+        }
+        buf
+    });
+
+    let status = child.wait().expect("Failed to get child status");
+    let stdout = out_handle
+        .join()
+        .expect("Failed to join threads")
+        .trim()
+        .to_string();
+    let stderr = err_handle
+        .join()
+        .expect("Failed to join threads")
         .trim()
         .to_string();
 
-    let utf8err = String::from_utf8(output.stderr)
-        .expect("Failed to decode stderr")
-        .trim()
-        .to_string();
+    let locale = sys_locale::get_locale()
+        .unwrap_or_else(|| String::from("en-US"))
+        .chars()
+        .take(2)
+        .collect::<String>();
 
-    println!("{}", utf8err);
+    println!("Program exited with status {}", status);
+
+    let prompt = format!(
+        "Exit code: {}\n\nSTDOUT:\n{}\n\nSTDERR:\n{}",
+        status, stdout, stderr
+    );
+
+    println!("{}", prompt);
 }
